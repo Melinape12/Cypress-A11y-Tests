0e3f611f27baf72820e398c85e1a5159bd9811a2
MelinaPerez <110926462+Melinape12@users.noreply.github.com>
2024-11-29 15:55:49 -0300

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -33,9 +33,16 @@ module.exports = {
     setupNodeEvents(on, config) {
       on("task", {
         logAccessibilityResults(results) {
-          const reportDir = path.join("cypress", "reports");
+          
+          console.log("Accessibility Results:", results);
 
           
+          const reportDir = path.join("cypress", "reports");
+
+          if (!fs.existsSync(reportDir)) {
+            fs.mkdirSync(reportDir, { recursive: true });
+          }
+
           fs.writeFileSync(
             path.join(reportDir, "accessibility.json"),
             JSON.stringify(results, null, 2)
