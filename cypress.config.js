d0171f15392f74602f43c2ab5c7499809610aef2
MelinaPerez <110926462+Melinape12@users.noreply.github.com>
2024-11-29 16:20:37 -0300

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,33 +1,6 @@
 const fs = require("fs");
 const path = require("path");
 
-const generateHTMLReport = (results) => {
-  const reportDir = path.join("cypress", "reports");
-
-  
-  if (!fs.existsSync(reportDir)) {
-    fs.mkdirSync(reportDir, { recursive: true });
-  }
-
-  let htmlContent = "<h1>Accessibility Audit Report</h1>";
-  results.forEach((result) => {
-    const affectedElements = result.nodes.map((node) => node.target).flat().join(", ");
-
-    htmlContent += `
-      <div>
-        <h2>Rule: ${result.rule}</h2>
-        <p>Description: ${result.description}</p>
-        <p>Impact: ${result.impact}</p>
-        <p>Help: <a href="${result.help}" target="_blank">More info</a></p>
-        <p>Affected elements: ${affectedElements}</p>
-      </div>
-    `;
-  });
-
-  
-  fs.writeFileSync(path.join(reportDir, "accessibility.html"), htmlContent);
-};
-
 module.exports = {
   e2e: {
     setupNodeEvents(on, config) {
@@ -43,13 +16,11 @@ module.exports = {
             fs.mkdirSync(reportDir, { recursive: true });
           }
 
+          
           fs.writeFileSync(
             path.join(reportDir, "accessibility.json"),
             JSON.stringify(results, null, 2)
           );
-
-          
-          generateHTMLReport(results);
 
           return null;
         },
